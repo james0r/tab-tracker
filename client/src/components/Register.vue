e2accde10b85a888b6a20e5cc51476e509428c83
James <james.auble@gmail.com>
2018-03-06 19:50:52 -0800

    Completed Part 2. Installed and set up simple panel with Vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,22 +1,31 @@
 <template>
 <!-- eslint-disable -->
-  <div>
-    <h1>Register</h1>
 
-    <input type="email"
-            name="email"
-            v-model="email"
-            placeholder="email" />
-            <br>
-    <input type="password"
-            name="password"
-            v-model="password"
-            placeholder="password" />
-            <br>
-            <button @click="register">
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input type="email"
+                    name="email"
+                    v-model="email"
+                    placeholder="email" />
+                    <br>
+            <input type="password"
+                    name="password"
+                    v-model="password"
+                    placeholder="password" />
+                    <br>
+                    <div class="error" v-html="error"></div>
+            <v-btn @click="register">
               Register
-            </button>
-  </div>
+            </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
@@ -24,20 +33,26 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register() {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error 
+      }
     }
   }
 }
 </script>
 <style>
-
+.error {
+  color: red;
+}
 </style>
